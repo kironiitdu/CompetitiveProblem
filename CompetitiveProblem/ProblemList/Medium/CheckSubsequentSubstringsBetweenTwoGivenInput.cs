2ecefe8e36d5bcd2b0c923ae6d7b5d304d230fc9
Md Farid Uddin Kiron <kironiitdu@outlook.com>
2024-06-04 14:56:55 +0800

    Check s1 is a sub string of s2

---
+++ CompetitiveProblem/ProblemList/Medium/CheckSubsequentSubstringsBetweenTwoGivenInput.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProblem.ProblemList.Medium
+{
+    public class CheckSubsequentSubstringsBetweenTwoGivenInput
+    {
+        public bool IsSubstring(string s1, string s2)
+        {
+            //Check the lengtg of s1 if length of s1 is zero then it can be a substring of any subsequent string
+            if (s1.Length == 0)
+            {
+                return true;
+            }
+            //If s1 is bigger than s2 then it cannot be a substring
+            if (s1.Length > s2.Length)
+            {
+                return false;
+            }
+            //Finding the length of outer loop for for s2
+            int outerBound = s2.Length - s1.Length;
+            for (int i = 0; i <= outerBound; i++)
+            {
+                //Inner loop for s1
+                int j;
+                for (j = 0; j < s1.Length; j++)
+                {
+                    // if the index of s1 is not the same of s2 index then its not a substrings
+                    if (s1[j] != s2[i+j])
+                    {
+                        break;
+                    }
+                }
+                //if the index of s1 and index of s2 is same then its a substring
+                if (j == s1.Length)
+                {
+                    return true;
+                }
+
+               
+            }
+
+            return false;
+        }
+    }
+}
--- CompetitiveProblem/Program.cs
+++ CompetitiveProblem/Program.cs
@@ -3,6 +3,7 @@
 using CompetitiveProblem.BigO._1.ConstantTime_O_1_;
 using CompetitiveProblem.BigO._2.Logarithmic_Time_O_logN_;
 using CompetitiveProblem.ProblemList.Easy.AlgorithomicProblems;
+using CompetitiveProblem.ProblemList.Medium;
 
 var twoSum = new TwoSumsProblem();
 int[] nums = { 2, 7, 11, 15 };
@@ -17,8 +18,16 @@ int newTarget = 1;
 //var output = testConstantBigO_N_1.AccessArrayElement(nums,newTarget);
 //Console.WriteLine("Expected Output should be 7 : {0}", output);
 
-var testNLogN = new O_logN_Logarithmic_Time();
-var result = testNLogN.BinarySearch(nums,target);
+//var testNLogN = new O_logN_Logarithmic_Time();
+//var result = testNLogN.BinarySearch(nums,target);
 
-Console.WriteLine("Expected Output should be 15 : {0}", result);
+//Console.WriteLine("Expected Output should be 15 : {0}", result);
+
+string s1 = "llo";
+string s2 = "hello world";
+
+// Check if str1 is a substring of str2
+var testSubstring = new CheckSubsequentSubstringsBetweenTwoGivenInput();
+bool isSubstring1 = testSubstring.IsSubstring(s1, s2);
+Console.WriteLine($"{s1} is a substring of {s2}: {isSubstring1}");
 
